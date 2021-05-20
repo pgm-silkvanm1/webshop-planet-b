371ce9ff9543fb130cc767474716e834187338f1
Bram Vandenbussche <bram.vandenbussche@live.be>
2021-05-20 13:29:40 +0200

    starting folder structure database

---
+++ server/src/models/product.model.js
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+	class Product extends Model {
+		static associate(models) {
+			this.belongsTo(models.Category);
+			this.belongsToMany(models.Tag, {
+				through: 'ProductTag',
+			});
+		}
+	}
+
+	Product.init(
+		{
+			name: DataTypes.STRING,
+			description: DataTypes.TEXT,
+			price: DataTypes.FLOAT,
+		},
+		{
+			sequelize,
+			modelName: 'Product',
+		},
+	);
+
+	return Product;
+};
---
+++ server/src/models/tag.model.js
@@ -0,0 +1,23 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+	class Tag extends Model {
+		static associate(models) {
+			this.belongsToMany(models.Product, {
+				through: 'ProductTag',
+			});
+		}
+	}
+
+	Tag.init(
+		{
+			name: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: 'Tag',
+		},
+	);
+
+	return Tag;
+};

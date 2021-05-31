9fea8fc9d7e9e0e18f166b8756753b44e13e2292
pgm-minajaid <71324864+pgm-minajaid@users.noreply.github.com>
2021-05-31 17:57:37 +0200

    responsivness

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -13,6 +13,7 @@
         this.$basket = document.querySelector('.basket__icon')
         this.$popUp = document.querySelector('.basket__popup')
         this.$close = document.querySelector('.basket__close')
+        this.$continue = document.querySelector('.basket__continue')
         this.$hamburger= document.querySelector('.nav__hamburger')
         this.$navList= document.querySelector('.nav__list')
       },
@@ -29,19 +30,23 @@
       },
 
       basketPopUp (){
-        console.log('test')
-        this.$basket.addEventListener("click",(evt)=> {
+     
+        this.$basket.addEventListener("click",()=> {
           this.$popUp.classList.add('open')
         }),
-        this.$close.addEventListener("click", (evt) => {
+        
+        this.$close.addEventListener("click", () => {
         this.$popUp.classList.remove('open');
+
+        })
+        this.$continue.addEventListener("click", () => {
+          this.$popUp.classList.remove('open');
         
       
       })
     },
 
     hamburgerMenu() {
-      console.log("it should work");
       this.$hamburger.addEventListener("click", (evt) => {
         this.$navList.classList.toggle("open");
       });

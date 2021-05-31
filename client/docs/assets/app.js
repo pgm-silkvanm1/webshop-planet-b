e10e8e1aee7fb5f2685b7fcdad77c2784aee1bf8
pgm-minajaid <71324864+pgm-minajaid@users.noreply.github.com>
2021-05-31 11:52:52 +0200

    hamburger menu

--- client/docs/assets/app.js
+++ client/docs/assets/app.js
@@ -6,12 +6,15 @@
         this.cacheElements();
         this.registerListeners();
         this.basketPopUp();
+        this.hamburgerMenu();
       },
       cacheElements() {
         this.toTop = document.querySelector('.to-top');
         this.$basket = document.querySelector('.basket__icon')
         this.$popUp = document.querySelector('.basket__popup')
         this.$close = document.querySelector('.basket__close')
+        this.$hamburger= document.querySelector('.nav__hamburger')
+        this.$navList= document.querySelector('.nav__list')
       },
 
   
@@ -30,15 +33,19 @@
         this.$basket.addEventListener("click",(evt)=> {
           this.$popUp.classList.add('open')
         }),
-      
-      
         this.$close.addEventListener("click", (evt) => {
         this.$popUp.classList.remove('open');
         
       
       })
-    }
-  
+    },
+
+    hamburgerMenu() {
+      console.log("it should work");
+      this.$hamburger.addEventListener("click", (evt) => {
+        this.$navList.classList.toggle("open");
+      });
+    },
 
     }
     app.init();

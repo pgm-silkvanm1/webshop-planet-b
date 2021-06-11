12daa012c4640708e2057c812ef17c54d26f3b92
pgm-minajaid <71324864+pgm-minajaid@users.noreply.github.com>
2021-06-11 15:26:06 +0200

    onclick products

--- client/docs/assets/app.js
+++ client/docs/assets/app.js
@@ -1,67 +1,58 @@
-
-
 (() => {
-    const app = {
-      init() {
-        this.cacheElements();
-        this.registerListeners();
-        this.basketPopUp();
-        this.hamburgerMenu();
-        this.webshopApi = new web();
-        this.printHomepage();
-        this.print();
-      },
-      cacheElements() {
-        this.toTop = document.querySelector('.to-top');
-        this.$basket = document.querySelector('.basket__icon')
-        this.$popUp = document.querySelector('.basket__popup')
-        this.$close = document.querySelector('.basket__close')
-        this.$continue = document.querySelector('.basket__continue')
-        this.$hamburger= document.querySelector('.nav__hamburger')
-        this.$navList= document.querySelector('.nav__list')
-        this.$products = document.querySelectorAll('.row__products')
-        this.$discount = document.querySelector('.row__discount')
-        this.$productList = document.querySelector('.product__container')
+  const app = {
+    init() {
+      this.cacheElements();
+      this.registerListeners();
+      this.basketPopUp();
+      this.hamburgerMenu();
+      this.webshopApi = new web();
+      this.printHomepage();
+      this.print();
+    },
+    cacheElements() {
+      this.toTop = document.querySelector(".to-top");
+      this.$basket = document.querySelector(".basket__icon");
+      this.$popUp = document.querySelector(".basket__popup");
+      this.$close = document.querySelector(".basket__close");
+      this.$continue = document.querySelector(".basket__continue");
+      this.$hamburger = document.querySelector(".nav__hamburger");
+      this.$navList = document.querySelector(".nav__list");
+      this.$products = document.querySelectorAll(".row__products");
+      this.$discount = document.querySelector(".row__discount");
+      this.$productList = document.querySelector(".product__container");
+      this.$navLink = document.querySelectorAll("nav .nav__list li a");
+    },
 
-        
-      },
+    registerListeners() {
+      this.toTop.addEventListener("click", () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      });
 
-  
-      registerListeners() {
-        this.toTop.addEventListener('click', () => {
-            window.scrollTo({
-                top: 0,
-                left: 0,
-                behavior: "smooth",
-            });
-        })
+      if (this.$productList !== null) {
+        this.$productList.addEventListener("click", (ev) => {
+          const basket =
+            ev.target.dataset.id ||
+            ev.target.parentNode.dataset.id ||
+            ev.target.parentNode.parentNode.dataset.id;
+          console.log(basket);
+        });
+      }
+    },
 
-        if(this.$productList !== null){
-          this.$productList.addEventListener('click', ev => {
-            const basket = ev.target.dataset.id || ev.target.parentNode.dataset.id || ev.target.parentNode.parentNode.dataset.id;
-            console.log(basket)
-        })
-
-        }
-        
-        
-      },
-
-      basketPopUp (){
-     
-        this.$basket.addEventListener("click",()=> {
-          this.$popUp.classList.add('open')
-        }),
-        
+    basketPopUp() {
+      this.$basket.addEventListener("click", () => {
+        this.$popUp.classList.add("open");
+      }),
         this.$close.addEventListener("click", () => {
-        this.$popUp.classList.remove('open');
-
-        })
-        this.$continue.addEventListener("click", () => {
-          this.$popUp.classList.remove('open');
-        
-      
-      })
+          this.$popUp.classList.remove("open");
+        });
+      this.$continue.addEventListener("click", () => {
+        this.$popUp.classList.remove("open");
+      });
     },
 
     hamburgerMenu() {
@@ -70,21 +61,29 @@
       });
     },
 
+    async printHomepage() {
+        
 
-    async printHomepage(){
+      this.$navLink.forEach((link)=>{
+        
+        console.log(link.getAttribute('href'))
+
+      })
+
+
       this.products = await this.webshopApi.getProducts();
-      let slicedProducts =[]
-      if(!!this.products){
-          slicedProducts = this.products.slice(1,7)
+      let slicedProducts = [];
+      if (!!this.products) {
+        slicedProducts = this.products.slice(1, 7);
       }
 
-      this.$products.forEach(element => {
-        element.innerHTML = slicedProducts.map((product)=>{
-         
-          return `
+      this.$products.forEach((element) => {
+        element.innerHTML = slicedProducts
+          .map((product) => {
+            return `
           <li>
             <a href = '/pages/detailpage' >
-              <img src="https://images.unsplash.com/photo-1622893795218-c3936a516616?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
+              <img src="${product.image}" loading="lazy" />
               
               <div class="product__main">
                 <p class = "product__main__name">${product.name}</p> 
@@ -93,22 +92,19 @@
 
         
             </a>
-          </li>`
-      }).join("");
-        
-        
+          </li>`;
+          })
+          .join("");
       });
-      
-  },
+    },
 
-  async print(){
-    this.productlist = await this.webshopApi.getKidsProducts();
-    console.log(this.productlist)
-    if(this.$productList !== null){
-
-      this.$productList.innerHTML = this.productlist.map(element => {
-        
-         return `
+    async print() {
+      this.productlist = await this.webshopApi.getKidsProducts();
+      console.log(this.productlist);
+      if (this.$productList !== null) {
+        this.$productList.innerHTML = this.productlist
+          .map((element) => {
+            return `
          <li>
            <div data-id="${element.id}">
              <svg xmlns="http://www.w3.org/2000/svg" width="37.048" height="33" viewBox="0 0 37.048 33">
@@ -117,7 +113,7 @@
            </div>
            <a href = '/pages/detailpage' >
     
-             <img src="https://images.unsplash.com/photo-1622893795218-c3936a516616?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" />
+             <img src="${element.image}" loading="lazy"/>
              
              <div class="product__main">
                <p class = "product__main__name">${element.name}</p> 
@@ -126,22 +122,11 @@
     
        
            </a>
-         </li>`
-     
-       
-       
-     }).join('');
-      
-    }
-},
-
-
-   
-
-
-    }
-    app.init();
-  })();
-
-
-
+         </li>`;
+          })
+          .join("");
+      }
+    },
+  };
+  app.init();
+})();
--- client/docs/assets/services.js
+++ client/docs/assets/services.js
@@ -37,6 +37,32 @@ function web() {
     }
   };
 
+  this.getCategories = async () => {   
+    try{
+      const response = await fetch (`${WEBSHOP_BASE_PATH}/products`);
+      const data = await response.json();
+      return data;
+      
+    } catch(error){
+      console.log('an error',error)
+
+    }
+  };
+
+  this.getCategoriesById = async (id) => {   
+    
+    try{
+      const response = await fetch (`${WEBSHOP_BASE_PATH}/products/${id}`);
+      const data = await response.json();
+      return data;
+      
+    } catch(error){
+      console.log('an error',error)
+
+    }
+  };
+
+
 
 
 
